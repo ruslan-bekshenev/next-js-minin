3d9285102e0feece550acb704bbba80513aeadf6
Ruslan Bekshenev <rbe@crtweb.ru>
2021-04-21 01:01:29 +0500

    Class Router

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,17 @@
-
+import { Fragment } from 'react'
+import Router from 'next/router'
 export default function Index() {
+
+  const linkClickHandler = (e, url = '/') => {
+    console.log(url)
+    Router.push(url)
+  }
+
   return (
-    <h1>About page</h1>
+    <Fragment>
+      <h1>About page</h1>
+      <button onClick={linkClickHandler}>Go back to home</button>
+      <button onClick={(e) => linkClickHandler(e, '/posts')}>Go to posts</button>
+    </Fragment>
   )
 }
