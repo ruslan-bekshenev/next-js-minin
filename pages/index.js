2363a343a4dfcd127f80eeb7ff263ccd76ce2cdd
Ruslan Bekshenev <rbe@crtweb.ru>
2021-04-21 00:55:40 +0500

    component link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 // React component
 
 export default function Index() {
+
   return <h1>
     Hello Next.JS!
+    <p><Link href={'/about'}><a>About</a></Link></p>
+    <p><Link href={'/posts'}><a>Posts</a></Link></p>
   </h1>
 }
